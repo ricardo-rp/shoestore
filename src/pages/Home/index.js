bd8c491a6a153868c5157c9d737b47ce38d0a9e5
Ricardo Romero <ricardo.romero@getmore.com.br>
2019-09-09 17:01:02 -0300

    Styled homepage

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,116 @@
 import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-import { Container } from "./styles";
+import { ProductList } from "./styles";
 
 export default function Home() {
-  return <Container> Home </Container>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong> Nome do tenis </strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong>Nome do tenis comprido que quebra duas linhas</strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong> Nome do tenis </strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong> Nome do tenis </strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong> Nome do tenis </strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong> Nome do tenis </strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
+          alt="shoe"
+        />
+        <strong> Nome do tenis </strong>
+        <span>R$123,45</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" />
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
