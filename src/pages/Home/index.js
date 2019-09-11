0a706814977712164d6a3dcde8f49086f5ad19cb
Ricardo Romero <ricardo.romero@getmore.com.br>
2019-09-11 11:13:08 -0300

    Home faz requisicoes de dados

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,116 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import { MdAddShoppingCart } from "react-icons/md";
+import api from "../../services/api";
+import { formatPrice } from "../../util/format";
 
 import { ProductList } from "./styles";
 
-export default function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong> Nome do tenis </strong>
-        <span>R$123,45</span>
+export default class Home extends Component {
+  state = {
+    products: []
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong>Nome do tenis comprido que quebra duas linhas</strong>
-        <span>R$123,45</span>
+  async componentDidMount() {
+    const response = await api.get("products");
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong> Nome do tenis </strong>
-        <span>R$123,45</span>
+    // Vamos evitar chamar o formatPrice() no render(), para ele nao rodar toda vez que algo mudar na pagina.
+    const data = response.data.map(product => ({
+      ...product,
+      priceFormatted: formatPrice(product.price)
+    }));
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong> Nome do tenis </strong>
-        <span>R$123,45</span>
+    this.setState({ products: data });
+  }
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong> Nome do tenis </strong>
-        <span>R$123,45</span>
+  render() {
+    const { products } = this.state;
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong> Nome do tenis </strong>
-        <span>R$123,45</span>
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://images-na.ssl-images-amazon.com/images/I/61DsWfBTvrL._UY500_.jpg"
-          alt="shoe"
-        />
-        <strong> Nome do tenis </strong>
-        <span>R$123,45</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" />
-          </div>
-          <span>ADICIONAR AO CARRINHO</span>
-        </button>
-      </li>
-    </ProductList>
-  );
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" />
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
