3574218687e2ddb10e695f9e1ff8109fa1252b83
moro <ricardoromero4444@gmail.com>
2019-09-16 00:49:07 -0300

    Signup works

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,21 +1,49 @@
 import React, { Component } from "react";
 
+import axios from "axios";
+
 import { Container } from "./styles";
+import api from "../../services/api";
 
 export default class SignUp extends Component {
+  state = {
+    name: "1",
+    email: "",
+    password: ""
+  };
+
+  handleButtonPress = event => {
+    api.post("/users", this.state);
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { email, password } = this.state;
+
     return (
       <Container>
-        <form>
+        <div>
           <strong> Cadastrar usuario </strong>
           <span>E-mail</span>
-          <input type="text"></input>
-          <span>Telefone</span>
-          <input type="text"></input>
+          <input
+            name="email"
+            onChange={this.handleInputChange}
+            type="text"
+          ></input>
           <span>Senha</span>
-          <input type="text"></input>
-          <button>Finalizar cadastro</button>
-        </form>
+          <input
+            name="password"
+            onChange={this.handleInputChange}
+            type="text"
+          ></input>
+          <button onClick={this.handleButtonPress}>Finalizar cadastro</button>
+        </div>
       </Container>
     );
   }
