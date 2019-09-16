19dfa61032b7da4444528b6edab65caa8cb7ebc9
moro <ricardoromero4444@gmail.com>
2019-09-15 23:49:06 -0300

    FINALMENTE funciona a requisicao de login

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,23 +1,58 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
+import api from "../../services/api";
+
 import { Container } from "./styles";
 
 export default class Login extends Component {
+  state = {
+    username: "",
+    password: ""
+  };
+
+  handleButtonPress = async event => {
+    const { username, password } = this.state;
+
+    const response = await api.post("/sessions", { email: username, password });
+
+    localStorage.setItem("token", response.data.token);
+  };
+
+  // movimento 200qi
+  handleInputChange = event => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value
+    });
+  };
+
   render() {
+    const { username, password } = this.state;
+
     return (
       <Container>
-        <form>
+        <div>
           <strong> Login</strong>
           <span>E-mail</span>
-          <input type="text"></input>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={this.handleInputChange}
+          ></input>
           <span>Senha</span>
-          <input type="password"></input>
-          <button>Log-in</button>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleInputChange}
+          ></input>
+          <button onClick={this.handleButtonPress}>Log-in</button>
           <Link to="/signup">
             <span>Não possui conta?</span>
           </Link>
-        </form>
+        </div>
       </Container>
     );
   }
--- src/pages/Login/styles.js
+++ src/pages/Login/styles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
 display: flex;
 justify-content: center;
 
-  form {
+  div {
     background: #fff;
     display: flex;
     flex-direction: column;
