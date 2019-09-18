0614407bfd4b33ceb68df36a8681a95024fc5b4c
moro <ricardoromero4444@gmail.com>
2019-09-18 01:18:33 -0300

    HOTFIX

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import withRouter from "react-router";
+import { withRouter } from "react-router";
 
 import api from "../../services/api";
 
